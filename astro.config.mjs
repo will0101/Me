cfc995aa8b74fc1a6c7a6737549d1c9e2676d35f
William Bogans <me@williambogans.dev>
2022-10-25 17:28:47 -0400

    Adds site compression just because

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()]
+  integrations: [sitemap(), compress()]
 });
